db36681cb56c9c4a979a2eaf2a4aadc53d029f87
stone <stone.shih@csnt.com.tw>
2019-12-11 11:54:59 +0800

    adjust type code format

--- src/assets/commonData/global.js
+++ src/assets/commonData/global.js
@@ -1,15 +1,18 @@
-export const yesNo = [{ key: 'Y', value: '是' }, { key: 'N', value: '否' }]
+export const yesNo = [
+  { key: 'Y', value: '是' },
+  { key: 'N', value: '否' }
+]
 
 export const assetType = [
-  { key: 'A', value: '銀行帳戶' },
-  { key: 'C', value: '信用卡' },
-  { key: 'M', value: '現金' },
-  { key: 'L', value: '貸款' },
-  { key: 'O', value: '其他' }
+  { key: 'Account', value: '銀行帳戶' },
+  { key: 'Credit', value: '信用卡' },
+  { key: 'Money', value: '現金' },
+  { key: 'Loan', value: '貸款' }
 ]
 
 export const otherAssetType = [
-  { key: 'S', value: '股票' },
-  { key: 'H', value: '房地產' },
-  { key: 'O', value: '其他' }
+  { key: 'Stock', value: '股票' },
+  { key: 'Insurence', value: '保險' },
+  { key: 'Estate', value: '房地產' },
+  { key: 'Other', value: '其他' }
 ]
